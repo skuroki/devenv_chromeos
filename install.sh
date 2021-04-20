7b33fcbe683bffc4dd33fbeab4137844ed808113
Shinsuke KUROKI <shinsukekuroki@gmail.com>
2021-04-21 01:35:08 +0900

    Split install script to ensure github key registration

--- install.sh
+++ install.sh
@@ -1,35 +1,8 @@
-#!/usr/bin/env bash -xe
-
 # -Nはパスフレーズの指定、-fはキーファイル名の指定
 mkdir .ssh
 ssh-keygen -N '' -f .ssh/id_rsa
 cat .ssh/id_rsa.pub 
 
-echo -n 'Githubにキーを登録したらEnterを押してください'
-read
-
-items=(
-  "rbenv"
-  "screen"
-  "gnome-terminal"
-  "zsh"
-  "neovim"
-)
-for item in "${items[@]}" ; do
-  sudo apt install -y ${item}
-done
-
-sudo chsh $USER -s $(which zsh)
-
-cd $HOME
-rm -rf dotfiles
-git clone git@github.com:skuroki/dotfiles.git
-cd dotfiles
-bash -x ./install.sh
-
-cd $HOME
-rm -rf .oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git config --global user.email "shinsukekuroki@gmail.com"
-git config --global user.name "Shinsuke KUROKI"
+echo 'Githubにキーを登録したら'
+echo 'curl -fsSL https://raw.githubusercontent.com/skuroki/dotfiles2/master/install2.sh | bash -x'
+echo 'を実行してください'
---
+++ install2.sh
@@ -0,0 +1,26 @@
+items=(
+  "rbenv"
+  "screen"
+  "gnome-terminal"
+  "zsh"
+  "neovim"
+)
+for item in "${items[@]}" ; do
+  sudo apt install -y ${item}
+done
+
+sudo chsh $USER -s $(which zsh)
+
+cd $HOME
+rm -rf dotfiles
+git clone git@github.com:skuroki/dotfiles.git
+cd dotfiles
+bash -x ./install.sh
+
+cd $HOME
+rm -rf .oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git config --global user.email "shinsukekuroki@gmail.com"
+git config --global user.name "Shinsuke KUROKI"
+
