fb6bfafc5aeb8c1d06b17594d56ae14c7114e937
KUROKI Shinsuke <shinsukekuroki@gmail.com>
2021-04-25 01:51:29 +0900

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -15,15 +15,14 @@ done
 sudo chsh $USER -s $(which zsh)
 
 cd $HOME
-rm -rf dotfiles
-git clone git@github.com:skuroki/dotfiles.git
-cd dotfiles
-bash -x ./install.sh
-
-cd $HOME
 rm -rf .oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+cd $HOME
+rm -rf dotfiles
+git clone git@github.com:skuroki/dotfiles.git
+cd dotfiles
+./install.sh
+
 git config --global user.email "shinsukekuroki@gmail.com"
 git config --global user.name "Shinsuke KUROKI"
-
