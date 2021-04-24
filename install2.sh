27a3bfe6254cdbd47cc8ed632df0d50eb8eaff95
KUROKI Shinsuke <shinsukekuroki@gmail.com>
2021-04-25 03:10:17 +0900

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -12,6 +12,8 @@ for item in "${items[@]}" ; do
 done
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+mkdir -p .rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git .rbenv/plugins/ruby-build
 
 sudo chsh $USER -s $(which zsh)
 
